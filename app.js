6d8934aec1198ac90594be9cd774b1452d5c58d2
Kaman Chamling <chamlingkaman@gmail.com>
2024-05-08 22:15:10 +0200

    add new feature

--- app.js
+++ app.js
@@ -8,28 +8,42 @@ const inputCity = document.querySelector("#input");
 const searchIcon = document.querySelector("#search-icon");
 const cityName = document.querySelector(".weather h2");
 const weatherIcon = document.querySelector(".weather img");
+const weather = document.querySelector(".weather");
+const details = document.querySelector(".details");
+const error = document.querySelector(".message p");
+const message = document.querySelector(".message");
+const container = document.querySelector(".container");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    error.style.display = "block";
+  } else {
+    message.remove()
+    weather.style.display = "flex";
+    details.style.display = "flex";
+  }
+
   var data = await response.json();
   console.dir(data.weather[0].main);
-
-  if (data.weather[0].main === "Clouds") {
-    weatherIcon.src = "./assets/images/clouds.png";
-  } else if (data.weather[0].main === "Clear") {
-    weatherIcon.src = "./assets/images/clear.png";
-  } else if (data.weather[0].main === "Rain") {
-    weatherIcon.src = "./assets/images/rain.png";
-  } else if (data.weather[0].main === "Drizzle") {
-    weatherIcon.src = "./assets/images/drizzle.png";
-  } else if (data.weather[0].main === "Mist") {
-    weatherIcon.src = "./assets/images/mist.png";
-  }
 
   cityName.innerText = data.name;
   temperature.innerText = Math.round(data.main.temp) + "°c";
   humidity.innerText = data.main.humidity + "%";
   wind.innerText = data.wind.speed + "km/h";
+
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
 }
 
 searchIcon.addEventListener("click", () => {
@@ -41,3 +55,4 @@ inputCity.addEventListener("keypress", (e) => {
     checkWeather(inputCity.value);
   }
 });
+inputCity.addEventListener("input", (e) => {});
