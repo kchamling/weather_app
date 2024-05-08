d434a0eb9b39f1bf7e531fefec6ba12411acdf2f
Kaman Chamling <chamlingkaman@gmail.com>
2024-05-08 12:55:05 +0200

    fetch api add

---
+++ app.js
@@ -0,0 +1,14 @@
+const apiKey = "a3c8b4d35c6ce1580408371d0f3cf57";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  const data = await response.json();
+
+  console.log(data);
+}
+
+checkWeather();
+
+console.log("hell");
